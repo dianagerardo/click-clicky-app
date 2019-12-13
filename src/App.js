ebe5ac0281450e41c5a6f24421261b2724d39e94
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-12-13 13:16:02 -0700

    Adding puppy pictures to puppyCard and puppy.json files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PuppyCard from "./components/PuppyCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import puppies from "./puppies.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    puppies
+  };
+
+  removePuppy = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const puppy = this.state.puppies.filter(puppy => puppy.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ puppies });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Puppies</Title>
+        {this.state.puppies.map(puppy => (
+          <PuppyCard
+            removePuppy={this.removePuppy}
+            id={puppy.id}
+            key={puppy.id}
+            name={puppy.name}
+            image={puppy.image}
+            occupation={puppy.occupation}
+            location={puppy.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
